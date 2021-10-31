f54d1c11225acf00f12a17c324dde6de0f50f53a
Thermazote <63803175+Thermazote@users.noreply.github.com>
2021-11-01 02:47:41 +0300

    Update web_parser.py. Now it can iterate pages with articles. Parsed to database first 5 pages of site, it's 90 articles.

--- web_parser/parser.py
+++ web_parser/parser.py
@@ -1,10 +1,14 @@
-import requests
 from bs4 import BeautifulSoup as BS
 import sqlite3
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-import time    
-    
+from selenium.webdriver.common.by import By
+import time
+
+def loadNewPage(driver, pageNumber):
+    driver.find_element(By.CLASS_NAME, "button-more").click()
+    driver.execute_script("arguments[0].setAttribute(arguments[1], arguments[2]);", driver.find_element(By.CLASS_NAME, "button-more"), "data-next-page", pageNumber)
+
 siteURL = "https://v102.ru"
 dbName = "db.sqlite"
 
@@ -12,12 +16,19 @@ dbName = "db.sqlite"
 db = sqlite3.connect(dbName)
 cursor = db.cursor()
 
-# get html page using webdriver
+# get html page using web driver
 chrome_options = Options()
-#chrome_options.add_argument("--headless")
+#chrome_options.add_argument("--headless")      # use background mode
 driver = webdriver.Chrome("chromedriver.exe", chrome_options=chrome_options)
 driver.get(siteURL)
-#time.sleep(0.1)
+
+# increase html page as many times as needed
+COUNT = 5   # count of times to press "show more articles"
+for it in range(0, COUNT):
+    loadNewPage(driver, it + 3)     # it + 3 because default next page is 2 so we should specify next number
+    #time.sleep(0.2)     # time to load articles, it'l be enough for 3 seconds 
+
+# get source code of page
 page = driver.page_source
 html = BS(page, "html.parser")
 
@@ -28,11 +39,11 @@ for article in rawArticles:
     # get article topic
     aName = article.find("h3").text
     aDate = article.find("span", class_="date-new").text
-    aLink = siteURL + article.find("a", class_ ="detail-link").get("href")
+    aLink = siteURL + article.find("a", class_="detail-link").get("href")
 
     # get article content
     driver.get(aLink)
-    #time.sleep(0.1)
+    # time.sleep(0.1)
     articlePage = driver.page_source
     aHtml = BS(articlePage, "html.parser")
     contentSection = aHtml.find("div", class_="n-text")
@@ -42,12 +53,10 @@ for article in rawArticles:
     if (playerSection is not None):
         aVideoLink = playerSection.find("iframe").get("src")
     aReplyCount = aHtml.find("span", class_="attr-comment").text
-    
-    # create dataset for current article
-    aDataSet = (aName, aDate, aLink, aText, aVideoLink, aReplyCount)
-    articles.append(aDataSet)
 
-# send articles to database    
-for article in articles:
-    cursor.execute("INSERT INTO articles(name, date, link, text, video_link, replies_count) VALUES(?,?,?,?,?,?)", article)
-db.commit()
+    # create dataset for current article and send it to database
+    aDataSet = (aName, aDate, aLink, aText, aVideoLink, aReplyCount)
+    cursor.execute("INSERT INTO articles(name, date, link, text, video_link, replies_count) VALUES(?,?,?,?,?,?)", aDataSet)
+    db.commit()
+
+driver.quit()
